c8ff065308560e65735906dc49d986886ec9d024
ShawnLi1014 <shawnli1014@gmail.com>
2019-06-24 18:20:36 +0800

    203, 237, 83

---
+++ 203.remove-linked-list-elements.java
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode id=203 lang=java
+ *
+ * [203] Remove Linked List Elements
+ *
+ * https://leetcode.com/problems/remove-linked-list-elements/description/
+ *
+ * algorithms
+ * Easy (35.86%)
+ * Likes:    853
+ * Dislikes: 51
+ * Total Accepted:    230.1K
+ * Total Submissions: 640.7K
+ * Testcase Example:  '[1,2,6,3,4,5,6]\n6'
+ *
+ * Remove all elements from a linked list of integers that have value val.
+ * 
+ * Example:
+ * 
+ * 
+ * Input:  1->2->6->3->4->5->6, val = 6
+ * Output: 1->2->3->4->5
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+       if (head == null) {
+           return head;
+       }
+       // If the first several node.val == val, move head, head = head.next
+       while(head.val == val && head != null) {
+           head = head.next;
+           if (head == null) {
+               return head;
+           }
+       }
+       // p.val is not val, start from p.next
+       ListNode p = head;
+       while (p.next != null) {
+           if (p.next.val == val) {
+                p.next = p.next.next;
+           } else {
+                p = p.next;
+           }
+       }
+       return head;
+    }
+}
+
---
+++ 83.remove-duplicates-from-sorted-list.java
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode id=83 lang=java
+ *
+ * [83] Remove Duplicates from Sorted List
+ *
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
+ *
+ * algorithms
+ * Easy (42.71%)
+ * Likes:    787
+ * Dislikes: 82
+ * Total Accepted:    335.3K
+ * Total Submissions: 784.1K
+ * Testcase Example:  '[1,1,2]'
+ *
+ * Given a sorted linked list, delete all duplicates such that each element
+ * appear only once.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 1->1->2
+ * Output: 1->2
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 1->1->2->3->3
+ * Output: 1->2->3
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode p = head;
+        while (p.next != null) {
+            if (p.val == p.next.val) {
+                p.next = p.next.next;
+            } else {
+                p = p.next;
+            }
+        }
+        return head;
+    }
+}
+
