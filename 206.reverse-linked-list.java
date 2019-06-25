862f23881ae62ceaca7b7e2751fb832d364eae01
ShawnLi1014 <shawnli1014@gmail.com>
2019-06-26 01:26:37 +0800

    206, 234

---
+++ 206.reverse-linked-list.java
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=206 lang=java
+ *
+ * [206] Reverse Linked List
+ *
+ * https://leetcode.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (55.02%)
+ * Likes:    2405
+ * Dislikes: 64
+ * Total Accepted:    608.3K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * Reverse a singly linked list.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: 1->2->3->4->5->NULL
+ * Output: 5->4->3->2->1->NULL
+ * 
+ * 
+ * Follow up:
+ * 
+ * A linked list can be reversed either iteratively or recursively. Could you
+ * implement both?
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode tempNext = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = tempNext;
+        }
+        return prev;
+    }
+}
+
---
+++ 234.palindrome-linked-list.java
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode id=234 lang=java
+ *
+ * [234] Palindrome Linked List
+ *
+ * https://leetcode.com/problems/palindrome-linked-list/description/
+ *
+ * algorithms
+ * Easy (36.21%)
+ * Likes:    1695
+ * Dislikes: 251
+ * Total Accepted:    267.4K
+ * Total Submissions: 736.9K
+ * Testcase Example:  '[1,2]'
+ *
+ * Given a singly linked list, determine if it is a palindrome.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 1->2
+ * Output: false
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 1->2->2->1
+ * Output: true
+ * 
+ * Follow up:
+ * Could you do it in O(n) time and O(1) space?
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null ) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if (fast != null) {
+            slow = slow.next;
+        }
+        slow = reverseList(slow);
+        fast = head;
+        while (slow != null) {
+            if (slow.val != fast.val) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
+    }
+
+    private ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode p = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
+    }
+}
+// Time complexity O(n), Space complexity O(1)
+
