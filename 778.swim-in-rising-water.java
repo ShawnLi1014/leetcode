c7686b1eeed7bbcdb6228b7e0941cc3a92ce9722
ShawnLi1014 <shawnli1014@gmail.com>
2019-07-26 02:21:37 +0800

    212, 778

---
+++ 778.swim-in-rising-water.java
@@ -0,0 +1,97 @@
+/*
+ * @lc app=leetcode id=778 lang=java
+ *
+ * [778] Swim in Rising Water
+ *
+ * https://leetcode.com/problems/swim-in-rising-water/description/
+ *
+ * algorithms
+ * Hard (47.71%)
+ * Likes:    325
+ * Dislikes: 32
+ * Total Accepted:    13.3K
+ * Total Submissions: 27.6K
+ * Testcase Example:  '[[0,2],[1,3]]'
+ *
+ * On an N x N grid, each square grid[i][j] represents the elevation at that
+ * point (i,j).
+ * 
+ * Now rain starts to fall. At time t, the depth of the water everywhere is t.
+ * You can swim from a square to another 4-directionally adjacent square if and
+ * only if the elevation of both squares individually are at most t. You can
+ * swim infinite distance in zero time. Of course, you must stay within the
+ * boundaries of the grid during your swim.
+ * 
+ * You start at the top left square (0, 0). What is the least time until you
+ * can reach the bottom right square (N-1, N-1)?
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [[0,2],[1,3]]
+ * Output: 3
+ * Explanation:
+ * At time 0, you are in grid location (0, 0).
+ * You cannot go anywhere else because 4-directionally adjacent neighbors have
+ * a higher elevation than t = 0.
+ * 
+ * You cannot reach point (1, 1) until time 3.
+ * When the depth of water is 3, we can swim anywhere inside the grid.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input:
+ * [[0,1,2,3,4],[24,23,22,21,5],[12,13,14,15,16],[11,17,18,19,20],[10,9,8,7,6]]
+ * Output: 16
+ * Explanation:
+ * ⁠0  1  2  3  4
+ * 24 23 22 21  5
+ * 12 13 14 15 16
+ * 11 17 18 19 20
+ * 10  9  8  7  6
+ * 
+ * The final route is marked in bold.
+ * We need to wait until time 16 so that (0, 0) and (4, 4) are connected.
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * 2 <= N <= 50.
+ * grid[i][j] is a permutation of [0, ..., N*N - 1].
+ * 
+ * 
+ */
+class Solution {
+    public int swimInWater(int[][] grid) {
+        int n = grid.length;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        int[][] visited = new int[n][n];
+        int[][] dirs = { { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 } };
+        pq.offer(new int[] { grid[0][0], 0, 0 });
+        visited[0][0] = 1;
+
+        while (!pq.isEmpty()) {
+            int[] point = pq.poll();
+            int elevation = point[0], y = point[1], x = point[2];
+            for (int[] dir : dirs) {
+                int newx = x + dir[0], newy = y + dir[1];
+                if (newx < 0 || newx >= n || newy < 0 || newy >= n) {
+                    continue;
+                }
+                if (visited[newy][newx] == 0) {
+                    visited[newy][newx] = 1;
+                    int maxElev = Math.max(elevation, grid[newy][newx]);
+                    if (newx == n - 1 && newy == n - 1) {
+                        return maxElev;
+                    }
+                    pq.offer(new int[] { maxElev, newy, newx });
+                }
+            }
+        }
+        return 0;
+    }
+}
+
