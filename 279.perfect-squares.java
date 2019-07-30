3b0d3079e8175ce092341998ef46f3645647e3fd
ShawnLi1014 <shawnli1014@gmail.com>
2019-07-30 22:49:38 +0800

    279 322 983 134

---
+++ 279.perfect-squares.java
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=279 lang=java
+ *
+ * [279] Perfect Squares
+ *
+ * https://leetcode.com/problems/perfect-squares/description/
+ *
+ * algorithms
+ * Medium (42.60%)
+ * Likes:    1570
+ * Dislikes: 128
+ * Total Accepted:    196.4K
+ * Total Submissions: 461.1K
+ * Testcase Example:  '12'
+ *
+ * Given a positive integer n, find the least number of perfect square numbers
+ * (for example, 1, 4, 9, 16, ...) which sum to n.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: n = 12
+ * Output: 3 
+ * Explanation: 12 = 4 + 4 + 4.
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: n = 13
+ * Output: 2
+ * Explanation: 13 = 4 + 9.
+ */
+class Solution {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, n + 1);
+        dp[0] = 0;
+        for (int i = 0; i <= Math.sqrt(n); i++) {
+            for (int j = i * i; j <= n; j++) {
+                dp[j] = Math.min(dp[j], dp[j - i * i] + 1);
+            }
+        }
+        return dp[n];
+    }
+}
+
